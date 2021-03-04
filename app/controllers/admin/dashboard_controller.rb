47f2519faf21277b449cb59f60922dc89b831220
BigBadAI <flashmefusion@hotmail.ca>
2021-03-04 01:43:57 +0000

    Update admin page to display categories and counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,19 @@ class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
   def show
-    
+    @products = Product.order(id: :desc).all
+    @counts = dashboard_counts @products
+  end
+
+  def dashboard_counts (items)
+    ans = {}
+    items.each do | i |
+      if (!ans[i.category.name])
+        ans.store("#{i.category.name}", 1)
+      else
+        ans[i.category.name] += 1
+      end
+    end
+    return ans
   end
 end
